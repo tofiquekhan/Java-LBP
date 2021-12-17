d8f5057905a83eae854036ba3a232b0782c80b63
Tofique Ahmed Khan <tofiquekhan171819@gmail.com>
2021-12-17 19:17:18 +0530

    SumOfOddDigits Problem

---
+++ Loops/SumOfOddDigits.java
@@ -0,0 +1,37 @@
+/*
+
+Implement a program to calculate sum of odd digits present in the given number
+
+Input Format
+
+a number from the user
+
+Constraints
+
+n>0
+
+Output Format
+
+print sum of odd digits
+
+*/
+import java.util.Scanner;
+public class SumOfOddDigits{
+
+public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+int num = sc.nextInt(),sum=0,digit;
+while(num!=0){
+digit = num%10;
+if(digit%2!=0){
+sum = sum+digit;
+}
+num = num/10;
+}
+System.out.println(sum);
+
+}
+
+
+}
